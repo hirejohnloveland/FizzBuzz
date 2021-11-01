0a5d7d711d01fbe1bca25768ebe736884315fdd7
Jordan Felan <felan.jordan94@gmail.com>
2021-11-01 12:01:46 -0500

    Finished base fizzbuzz TDD now moving to control flowand to check if is integer

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,26 @@
 public class FizzBuzz implements IFizzBuzz{
     public String generate(int input){
-        if(input == 2){
-            return "1,2";
+        String result = "";
+
+        for(int i = 1; i <= input; i++){
+
+            if(i % 15 == 0){
+                result += "FizzBuzz";
+            }
+            else if(i % 3 == 0){
+                result += "Fizz";
+            }
+            else if(i % 5 == 0){
+                result += "Buzz";
+            } else {
+                result += Integer.toString(i);
+            }
+            if(i != input){
+                result += ",";
+            }
+
         }
 
-        return "1";
+        return result;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -22,8 +22,85 @@ class FizzBuzzTest {
 //        When: I enter the number 2
         IFizzBuzz fb = new FizzBuzz();
         String result = fb.generate(2);
-//        Then: I get back "1"
-        assertEquals("1, 2", result);
+//        Then: I get back "1,2"
+        assertEquals("1,2", result);
+    }
+
+    @Test
+    public void given3Get1_2_Fizz() {
+//        Given: I
+//        am a user
+//        When: I enter the number 3
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(3);
+//        Then: I get back "1,2,Fizz"
+        assertEquals("1,2,Fizz", result);
+    }
+
+    @Test
+    public void given4Get1_2_Fizz_4() {
+//        Given: I
+//        am a user
+//        When: I enter the number 4
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(4);
+//        Then: I get back "1,2,Fizz,4"
+        assertEquals("1,2,Fizz,4", result);
+    }
+
+    @Test
+    public void given5EndsWithBuzz() {
+//        Given: I
+//        am a user
+//        When: I enter the number 5
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(5);
+//        Then: I get back a string ending with "Buzz"
+        assertTrue(result.endsWith("Buzz"));
+    }
+
+    @Test
+    public void given6EndsWithFizz() {
+//        Given: I
+//        am a user
+//        When: I enter the number 6
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(6);
+//        Then: I get back a string ending with "Fizz"
+        assertTrue(result.endsWith("Fizz"));
+    }
+
+    @Test
+    public void given10EndsWithBuzz() {
+//        Given: I
+//        am a user
+//        When: I enter the number 10
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(10);
+//        Then: I get back a string ending with "Buzz"
+        assertTrue(result.endsWith("Buzz"));
+    }
+
+    @Test
+    public void given15EndsWithFizzBuzz() {
+//        Given: I
+//        am a user
+//        When: I enter the number 15
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(15);
+//        Then: I get back a string ending with "FizzBuzz"
+        assertTrue(result.endsWith("FizzBuzz"));
+    }
+
+    @Test
+    public void given30EndsWithFizzBuzz() {
+//        Given: I
+//        am a user
+//        When: I enter the number 30
+        IFizzBuzz fb = new FizzBuzz();
+        String result = fb.generate(30);
+//        Then: I get back a string ending with "FizzBuzz"
+        assertTrue(result.endsWith("FizzBuzz"));
     }
 
 }
